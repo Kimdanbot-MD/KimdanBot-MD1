318b5dd3f200166f42f78639463083edac8f216f
Kimdanbot-MD <138764603+Kimdanbot-MD@users.noreply.github.com>
2024-03-20 06:46:12 -0500

    Update index.js

--- index.js
+++ index.js
@@ -608,7 +608,6 @@ body: wm,
 //nuevo admin
 } else if (anu.action == "promote") {
 const buffer = await getBuffer(ppuser)
-const} else if (anu.action == 'promote') {
 //let users = participants.map(u => sock.decodeJid(u.id))
 const groupAdmins = participants.filter(p => p.admin)
 const listAdmin = groupAdmins.map((v, i) => `*» ${i + 1}. @${v.id.split('@')[0]}*`).join('\n')
