d47ef13fb2dbf2b7ee796d94f81065507605de87
Kimdanbot-MD <138764603+Kimdanbot-MD@users.noreply.github.com>
2024-03-23 23:06:48 -0500

    Update kim.js

--- kim.js
+++ kim.js
@@ -1267,7 +1267,7 @@ conn.copyNForward(m.chat, msgs[budy.toLowerCase()], true)
  
 // ═════════════𓊈『 REPORTE/ERRORES 』𓊉═════════════
 let e = String(err) 
-conn.sendMessage("5492266466080@s.whatsapp.net", { text: "Hola Creador/desarrollador, parece haber un error, por favor arreglarlo 🥲\n\n" + util.format(e), 
+conn.sendMessage("573173090446@s.whatsapp.net", { text: "Hola Creador/desarrollador, parece haber un error, por favor arreglarlo 🥲\n\n" + util.format(e), 
 contextInfo:{forwardingScore: 9999999, isForwarded: false }})
 process.on('uncaughtException', function (err) {
 console.log('Caught exception: ', err)})
