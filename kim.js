926dc603405930a0b7905c2c5f3e2abef19501b7
@Azami <141449338+AzamiJs@users.noreply.github.com>
2024-04-03 18:50:18 -0600

    Update kim.js

--- kim.js
+++ kim.js
@@ -508,7 +508,7 @@ break
 let pp = await conn.profilePictureUrl(m.chat, 'image')	
 await conn.sendMessage(m.chat, pp) 		
 	break
-case 'serbot': case 'qr':
+/*case 'serbot': case 'qr':
 jadibot(conn, m, command)  
 break  
 //case 'jadibot': case 'sercode':
@@ -524,7 +524,7 @@ const replyMessage = message.length === 0 ? '' : message;
 const totalUsers = user.length;
 const responseMessage = `*𝘚𝘜𝘉𝘉𝘖𝘛𝘚 𝘊𝘖𝘕𝘌𝘊𝘛𝘈𝘋𝘖𝘚:* ${totalUsers || '0'}\n\n${replyMessage.trim()}`.trim();
 await conn.sendMessage(m.chat, {text: responseMessage, mentions: conn.parseMention(responseMessage)}, {quoted: m});
-break
+break*/
 		
 // 𝐈𝐍𝐅𝐎𝐑𝐌𝐀𝐂𝐈𝐎𝐍 𝐃𝐄 𝐊𝐈𝐌𝐃𝐀𝐍𝐁𝐎𝐓-𝐌𝐃
 case 'estado': case 'heydan': case 'status':
