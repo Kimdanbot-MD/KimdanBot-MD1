c94dc4f2664a9e1f7a749b5cfeafb9b34113c74f
Kimdanbot-MD <138764603+Kimdanbot-MD@users.noreply.github.com>
2024-04-07 21:29:13 -0500

    Update settings.js

--- settings.js
+++ settings.js
@@ -82,9 +82,9 @@ global.tiktok = 'https://www.tiktok.com/@universo_yaoi_bl?_t=8iIlNrlJg1d&_r=1'
 global.fb = 'https://www.instagram.com/_universo.bl?igshid=OGQ5ZDc2ODk2ZA=='
 
 // No mames Jose
-const red = [["md"], ["yt"], ["tiktok"], ["fb"]]
+const red = [md, yt, tiktok, fb]
 const xd = red[Math.floor(Math.random() * red.length)]; 
-global.redes = xd 
+global.redes = `${xd}`
 
 global.nna = 'https://whatsapp.com/channel/0029VaFFJab3QxS5sqmnXR3l' //canal
 global.nn = 'https://chat.whatsapp.com/C0lYCnklEtg1HUkbR4uPxA' //kim
