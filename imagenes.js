4e76e6b2949e4af0241fd91805a26c8be2587df6
@Zam <141449338+AzamiJs@users.noreply.github.com>
2024-04-13 21:29:26 -0600

    .

---
+++ imagenes.js
@@ -0,0 +1,13 @@
+//Imágenes 
+
+let img1 = 'https://telegra.ph/file/6ef00a79a7c90c05e7043.jpg'
+let img2 = 'https://telegra.ph/file/9303b6e2f8c538b3e60c3.jpg'
+let img3 = 'https://telegra.ph/file/9cf56c843c5ae30259542.jpg'
+let img4 = 'https://telegra.ph/file/b71adbc68373626812de3.jpg'
+let img5 = 'https://telegra.ph/file/fb3422d7bf3df842bfe16.jpg'
+let img6 = 'https://telegra.ph/file/4fe23b2f1dfbf9e228166.jpg'
+let img7 = 'https://telegra.ph/file/9a72dc7935d93ae36f8a2.jpg'
+let img8 = 'https://telegra.ph/file/9b93ce7337f602bb61736.jpg'
+let img9 = 'https://telegra.ph/file/ee5dcfd3e79ef229c14d0.jpg'
+
+global.fotos = [img1, img2, img3, img4, img5, img6, img7, img8, img9]
