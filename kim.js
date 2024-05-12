2a271110807bc99a7d32043585291ffd731c64cb
Kimdanbot-MD <138764603+Kimdanbot-MD@users.noreply.github.com>
2024-05-11 22:14:30 -0500

    Update kim.js

--- kim.js
+++ kim.js
@@ -497,7 +497,7 @@ return conn.sendFile(m.chat, buffer, 'error.jpg', `${msg[type].caption} ${teks}`
 }}
 //falta tictactoe y los otros antilink
 	
-switch (command) {
+switch (prefix && command) { 
 
 case 'plist':
 conn.relayMessage(from, { viewOnceMessage: { message: { interactiveMessage: { header: { title: 'Lista' }, body: { text: '💤' }, nativeFlowMessage: { buttons: [ { name: 'single_select', buttonParamsJson: JSON.stringify({ title: 'Click', sections: [ {title: 'Lista', highlight_label: 'Yaoi', rows: [{ title: 'menu1', id: 'menu1' }]}, { highlight_label: 'ON', rows: [{ header: 'Test', title: 's',description: 's', id: 'tes'}] }, { highlight_label: 'ON', rows: [ { header: 'Test', title: 'status', description: 'status', id: 'te' }]}]})}], messageParamsJson: '' }}}}}, {})		
