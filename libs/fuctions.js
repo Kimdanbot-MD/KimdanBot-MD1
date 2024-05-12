617e34d0ffb3fb454a0f2ab00e225e361f56171e
Ziooo <103476365+Alexito-Hub@users.noreply.github.com>
2024-05-12 11:21:33 -0500

    Update fuctions.js

--- libs/fuctions.js
+++ libs/fuctions.js
@@ -865,7 +865,45 @@ conn.sendPayment = async (jid, amount, text, quoted, options) => {
     * @param {*} orderTitle // optional
     * @param {*} userJid // optional
     */
-    
+    conn.waitMessage = async (options = {}) => {
+		return new Promise((resolve, reject) => {
+			if (typeof options !== 'object') reject(new Error('Options must be an object'));
+            if (typeof options.sender !== 'string') reject(new Error('Sender must be a string'));
+            if (typeof options.chatJid !== 'string') reject(new Error('ChatJid must be a string'));
+            if (options.timeout && typeof options.timeout !== 'number') reject(new Error('Timeout must be a number'));
+            if (options.filter && typeof options.filter !== 'function') reject(new Error('Filter must be a function'));
+            
+            const timeout = options?.timeout || undefined;
+            const filter = options?.filter || (() => true);
+            let interval = undefined
+                    
+            let listener = (data) => {
+                let { type, messages } = data;
+				if (type == "notify") {
+					for (let message of messages) {
+                        const fromMe = message.key.fromMe;
+                        const chatId = message.key.remoteJid;
+                        const isGroup = chatId.endsWith('@g.us');
+                        const isStatus = chatId == 'status@broadcast';
+            
+                        const sender = fromMe ? sock.user.id.replace(/:.*@/g, '@') : (isGroup || isStatus) ? message.key.participant.replace(/:.*@/g, '@') : chatId;
+                        if (sender == options.sender && chatId == options.chatJid && filter(message)) {
+                            sock.ev.off('messages.upsert', listener);
+                            clearTimeout(interval);
+                            resolve(message);
+                        }
+                    }
+                }
+            }
+            sock.ev.on('messages.upsert', listener);
+            if (timeout) {
+                interval = setTimeout(() => {
+                    socket.ev.off('messages.upsert', listener);
+                    reject(new Error('Timeout'));
+                }, timeout);
+            }
+        });
+    }
     conn.sendCart = async (jid, text, thumbail, orderTitle, userJid) => {
     var messa = await prepareWAMessageMedia({ image: thumbail ? thumbail : success }, { upload: conn.waUploadToServer })
     var order = generateWAMessageFromContent(jid, proto.Message.fromObject({
