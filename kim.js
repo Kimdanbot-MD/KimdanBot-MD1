2fae84914ca56f005310e88fcfe6124b99e16100
Kimdanbot-MD <138764603+Kimdanbot-MD@users.noreply.github.com>
2024-05-14 12:17:37 -0500

    Update kim.js

--- kim.js
+++ kim.js
@@ -535,8 +535,6 @@ break
 case 'idioma': {
 let settings = global.db.data.settings[conn.user.jid]
 if (!text) return m.reply(`a que idioma cambio?`) 
-const sendLanguage = generateWAMessageFromContent(conn.user.id.split(":")[0] + "@s.whatsapp.net", { viewOnceMessage: { message: { "messageContextInfo": { "deviceListMetadata": {}, "deviceListMetadataVersion": 2 }, interactiveMessage: proto.Message.InteractiveMessage.create({ body: proto.Message.InteractiveMessage.Body.create({ text: '' }), footer: proto.Message.InteractiveMessage.Footer.create({ text: '' }), header: proto.Message.InteractiveMessage.Header.create({ title: 'Hello, thank you for using our bot, now, there are only a few steps left to finish, please select your preferred language.', subtitle: 'select an option.', hasMediaAttachment: false }), nativeFlowMessage: proto.Message.InteractiveMessage.NativeFlowMessage.create({ buttons: [ { "name": "quick_reply", "buttonParamsJson": `{"display_text":"Español","id":".idioma es"}` }, { "name": "quick_reply", "buttonParamsJson": `{"display_text":"English","id":".idioma en"}` } ], })})}}}, {})
-conn.relayMessage(sendLanguage.key.remoteJid, sendLanguage.message, { messageId: sendLanguage.key.id })
 if (budy.includes(`1`)) { 
 idioma = 'es' 
 idiomas = 'español'
@@ -546,6 +544,8 @@ idioma = 'en'
 idiomas = 'ingles'
 }
 settings.Language = idioma
+const sendLanguage = generateWAMessageFromContent(conn.user.id.split(":")[0] + "@s.whatsapp.net", { viewOnceMessage: { message: { "messageContextInfo": { "deviceListMetadata": {}, "deviceListMetadataVersion": 2 }, interactiveMessage: proto.Message.InteractiveMessage.create({ body: proto.Message.InteractiveMessage.Body.create({ text: '' }), footer: proto.Message.InteractiveMessage.Footer.create({ text: '' }), header: proto.Message.InteractiveMessage.Header.create({ title: 'Hello, thank you for using our bot, now, there are only a few steps left to finish, please select your preferred language.', subtitle: 'select an option.', hasMediaAttachment: false }), nativeFlowMessage: proto.Message.InteractiveMessage.NativeFlowMessage.create({ buttons: [ { "name": "quick_reply", "buttonParamsJson": `{"display_text":"Español","id":".idioma es"}` }, { "name": "quick_reply", "buttonParamsJson": `{"display_text":"English","id":".idioma en"}` } ], })})}}}, {})
+conn.relayMessage(sendLanguage.key.remoteJid, sendLanguage.message, { messageId: sendLanguage.key.id })
 m.reply(`se cambio a ` + idiomas)}
 break 
 		
