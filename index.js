7941851e7dd5372d42122a2087652529bbefea2a
Kimdanbot-MD <138764603+Kimdanbot-MD@users.noreply.github.com>
2024-06-07 21:14:07 -0500

    Update index.js

--- index.js
+++ index.js
@@ -555,6 +555,122 @@ mentionedJid:[m.sender],
 "mediaUrl": redes,  
 "sourceUrl": canales 
 }}}, {quoted: null, ephemeralExpiration: 24*60*100, disappearingMessagesInChat: 24*60*100})
+} else if (res.isCommunity == true) {
+await sleep(2000)
+try {
+ppgroup = await sock.profilePictureUrl(res.id, 'image')
+} catch (err) {
+ppgroup = 'https://i.ibb.co/RBx5SQC/avatar-group-large-v2.png'
+}
+//todos pueden AGG gente
+sock.sendMessage(res.id, {text: lenguaje['smsAvisos10'](),  
+contextInfo:{  
+forwardedNewsletterMessageInfo: { 
+newsletterJid: '120363200204060894@newsletter', 
+serverMessageId: '', 
+newsletterName: '༻꫞⃝🧃 𝐊𝐢𝐦𝐝𝐚𝐧𝐁𝐨𝐭-𝐌𝐃 🧃⃝꫞༺' },
+forwardingScore: 9999999,
+isForwarded: true,
+remoteJid: res.id,
+mentionedJid:[m.sender],  
+"externalAdReply": {  
+"showAdAttribution": true,  
+"containsAutoReply": false,
+"renderLargerThumbnail": false,  
+"title": lenguaje['smsAvisos5'](),
+"body": wm, 
+"mediaType": 1,   
+"thumbnailUrl": ftkim,  
+"mediaUrl": redes,  
+"sourceUrl": canales 
+}}}, {quoted: null, ephemeralExpiration: 24*60*100, disappearingMessagesInChat: 24*60*100})
+} else if (res.isCommunity == false) {
+await sleep(2000)
+try {
+ppgroup = await sock.profilePictureUrl(res.id, 'image')
+} catch (err) {
+ppgroup = 'https://i.ibb.co/RBx5SQC/avatar-group-large-v2.png'
+}
+//solo admins
+sock.sendMessage(res.id, {text: lenguaje['smsAvisos11'](),  
+contextInfo:{  
+forwardedNewsletterMessageInfo: { 
+newsletterJid: '120363200204060894@newsletter', 
+serverMessageId: '', 
+newsletterName: '༻꫞⃝🧃 𝐊𝐢𝐦𝐝𝐚𝐧𝐁𝐨𝐭-𝐌𝐃 🧃⃝꫞༺' },
+forwardingScore: 9999999,
+isForwarded: true,
+remoteJid: res.id,
+mentionedJid:[m.sender],  
+"externalAdReply": {  
+"showAdAttribution": true,  
+"containsAutoReply": false,
+"renderLargerThumbnail": false,  
+"title": lenguaje['smsAvisos5'](),
+"body": wm, 
+"mediaType": 1,   
+"thumbnailUrl": ftkim,  
+"mediaUrl": redes,  
+"sourceUrl": canales 
+}}}, {quoted: null, ephemeralExpiration: 24*60*100, disappearingMessagesInChat: 24*60*100})
+} else if (res.isCommunityAnnounce == true) {
+await sleep(2000)
+try {
+ppgroup = await sock.profilePictureUrl(res.id, 'image')
+} catch (err) {
+ppgroup = 'https://i.ibb.co/RBx5SQC/avatar-group-large-v2.png'
+}
+//todos pueden AGG gente
+sock.sendMessage(res.id, {text: lenguaje['smsAvisos10'](),  
+contextInfo:{  
+forwardedNewsletterMessageInfo: { 
+newsletterJid: '120363200204060894@newsletter', 
+serverMessageId: '', 
+newsletterName: '༻꫞⃝🧃 𝐊𝐢𝐦𝐝𝐚𝐧𝐁𝐨𝐭-𝐌𝐃 🧃⃝꫞༺' },
+forwardingScore: 9999999,
+isForwarded: true,
+remoteJid: res.id,
+mentionedJid:[m.sender],  
+"externalAdReply": {  
+"showAdAttribution": true,  
+"containsAutoReply": false,
+"renderLargerThumbnail": false,  
+"title": lenguaje['smsAvisos5'](),
+"body": wm, 
+"mediaType": 1,   
+"thumbnailUrl": ftkim,  
+"mediaUrl": redes,  
+"sourceUrl": canales 
+}}}, {quoted: null, ephemeralExpiration: 24*60*100, disappearingMessagesInChat: 24*60*100})
+} else if (res.isCommunityAnnounce == false) {
+await sleep(2000)
+try {
+ppgroup = await sock.profilePictureUrl(res.id, 'image')
+} catch (err) {
+ppgroup = 'https://i.ibb.co/RBx5SQC/avatar-group-large-v2.png'
+}
+//test
+sock.sendMessage(res.id, {text: lenguaje['smsAvisos11'](),  
+contextInfo:{  
+forwardedNewsletterMessageInfo: { 
+newsletterJid: '120363200204060894@newsletter', 
+serverMessageId: '', 
+newsletterName: '༻꫞⃝🧃 𝐊𝐢𝐦𝐝𝐚𝐧𝐁𝐨𝐭-𝐌𝐃 🧃⃝꫞༺' },
+forwardingScore: 9999999,
+isForwarded: true,
+remoteJid: res.id,
+mentionedJid:[m.sender],  
+"externalAdReply": {  
+"showAdAttribution": true,  
+"containsAutoReply": false,
+"renderLargerThumbnail": false,  
+"title": lenguaje['smsAvisos5'](),
+"body": wm, 
+"mediaType": 1,   
+"thumbnailUrl": ftkim,  
+"mediaUrl": redes,  
+"sourceUrl": canales 
+}}}, {quoted: null, ephemeralExpiration: 24*60*100, disappearingMessagesInChat: 24*60*100})
 } else if(!res.desc == ''){
 await sleep(2000)
 try {
