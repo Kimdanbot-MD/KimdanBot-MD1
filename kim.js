099b4a7665d570bf2adcb30a87e6d55dfb291f43
Kimdanbot-MD <138764603+Kimdanbot-MD@users.noreply.github.com>
2024-06-10 03:28:03 -0500

    Update kim.js

--- kim.js
+++ kim.js
@@ -611,10 +611,9 @@ delete this.confirm[sender];
 //*. : ｡✿ * ﾟ * .: ｡ ✿ * ﾟ  * . : ｡ ✿ *
 
 switch (command) { 
-	case 'prueba':
-		conn.sendMessage(m.chat, {text: getFormattedBookList}, {quoted: m, ephemeralExpiration: 24*60*100, disappearingMessagesInChat: 24*60*100})
-		break 
-		
+case 'prueba':
+getFormattedBookList()		
+break
 		
 case 'plist':
 conn.relayMessage(from, { viewOnceMessage: { message: { interactiveMessage: { header: { title: 'Lista' }, body: { text: '💤' }, nativeFlowMessage: { buttons: [ { name: 'single_select', buttonParamsJson: JSON.stringify({ title: 'Click', sections: [ {title: 'Lista', highlight_label: 'Yaoi', rows: [{ title: 'menu1', id: 'menu1' }]}, { highlight_label: 'ON', rows: [{ header: 'Test', title: 's',description: 's', id: 'tes'}] }, { highlight_label: 'ON', rows: [ { header: 'Test', title: 'status', description: 'status', id: 'te' }]}]})}], messageParamsJson: '' }}}}}, {})		
