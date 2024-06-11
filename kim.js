945876d50f3e1cb4596923c91e7b219466155a97
Kimdanbot-MD <138764603+Kimdanbot-MD@users.noreply.github.com>
2024-06-11 01:17:02 -0500

    Update kim.js

--- kim.js
+++ kim.js
@@ -618,7 +618,10 @@ break
 case 'libro':
 searchBooks(text, conn, m, from)		
 break
-
+case 'agglibro':
+addBook(text, conn, m, from)
+break
+		
 case 'plist':
 conn.relayMessage(from, { viewOnceMessage: { message: { interactiveMessage: { header: { title: 'Lista' }, body: { text: '💤' }, nativeFlowMessage: { buttons: [ { name: 'single_select', buttonParamsJson: JSON.stringify({ title: 'Click', sections: [ {title: 'Lista', highlight_label: 'Yaoi', rows: [{ title: 'menu1', id: 'menu1' }]}, { highlight_label: 'ON', rows: [{ header: 'Test', title: 's',description: 's', id: 'tes'}] }, { highlight_label: 'ON', rows: [ { header: 'Test', title: 'status', description: 'status', id: 'te' }]}]})}], messageParamsJson: '' }}}}}, {})		
 break 
