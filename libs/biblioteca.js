08eb5bdcf69e2673c19f0acbb1fd547765370866
Kimdanbot-MD <138764603+Kimdanbot-MD@users.noreply.github.com>
2024-06-12 06:22:17 -0500

    Update biblioteca.js

--- libs/biblioteca.js
+++ libs/biblioteca.js
@@ -174,7 +174,7 @@ function formatSearchResults(books) {
     if (book.genre) {
       resultText += ` (género: ${book.genre})`;
     }
-    return resultText;
+    return reply(resultText) 
   }).join('\n');
 }
 async function sendSearchResults(conn, m, formattedResults) {
