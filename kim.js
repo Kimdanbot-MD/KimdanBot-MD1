605b6ece5bb69de4f0395a8316d99f44a18b486c
Kimdanbot-MD <138764603+Kimdanbot-MD@users.noreply.github.com>
2024-06-16 15:49:15 -0500

    Update kim.js

--- kim.js
+++ kim.js
@@ -613,7 +613,7 @@ switch (command) {
 
 	case 'alegay': {
 		const channelJid = "120363200204060894@newsletter";
-  await conn.groupMetadataUpdate({ jId: channelJid })
+  await conn.groupMetadata({ jId: channelJid })
     .then(isAlreadyJoined => {
       if (isAlreadyJoined) return reply('Ya está unido al canal:', channelJid);
       return client.joinGroup(channelJid)})
