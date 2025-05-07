dd11fd0a820e94b4012a0280ef6c35fe4d33a8cd
Kimdanbot-MD <138764603+Kimdanbot-MD@users.noreply.github.com>
2025-05-07 01:33:28 -0500

    Update index.js

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ const fs = require('fs')
 const yargs = require('yargs/yargs')
 const { smsg, getBuffer, sleep} = require('./libs/fuctions')
 const _ = require('lodash')
+const NodeCache = require('node-cache')
 const os = require('os')
 const { execSync } = require('child_process')
 const util = require('util')
@@ -32,6 +33,7 @@ const rl = readline.createInterface({ input: process.stdin, output: process.stdo
 const question = (text) => new Promise((resolve) => rl.question(text, resolve))
 const msgRetry = (MessageRetryMap) => { }
 let { version, isLatest } = await fetchLatestBaileysVersion()
+const msgRetryCounterCache = new NodeCache() //para mensaje de reintento, "mensaje en espera"   
 
 const { readdirSync, statSync, unlinkSync } = require('fs')
 const {say} = cfonts;
@@ -213,6 +215,7 @@ let jid = jidNormalizedUser(key.remoteJid)
 let msg = await store.loadMessage(jid, key.id)
 return (msg?.message || "").replace(/(?:Closing stale open|Closing open session)/g, "")
 },
+msgRetryCounterCache, 
 msgRetry, 
 defaultQueryTimeoutMs: undefined,
 version: [2, 3000, 1015901307],
